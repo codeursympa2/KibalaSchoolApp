b1e2b5fba8be1817d7ef54e0904cca77a5bcc999
Anrichidine Abdallah <abdouanjouanais@gmail.com>
2021-05-01 18:57:25 +0000

    Introduction des fichiers static et activation du design

--- KibalaSchoolApp/urls.py
+++ KibalaSchoolApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from authentification.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',login_blog,name="home"),
 ]
+
